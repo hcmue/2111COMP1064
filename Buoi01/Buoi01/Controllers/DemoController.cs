ababbee350e85a2ea0ee96699eb598c57a55e664
Hien Luong <hyhien@gmail.com>
2021-09-09 08:01:10 +0700

    Init project

---
+++ Buoi01/Buoi01/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Buoi01.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult XoSo()
+        {
+            var rd = new Random();
+            var danhSach = new List<int>();
+            for (int i = 0; i < 6; i++)
+            {
+                danhSach.Add(rd.Next(1, 55));
+            }
+
+            return View();
+        }
+    }
+}
