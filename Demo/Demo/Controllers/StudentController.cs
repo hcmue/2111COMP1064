95dbe4cd9f689793b75b36381b2baee5a403ab61
Hien Luong <hyhien@gmail.com>
2021-09-16 09:09:38 +0700

    Demo Doc Ghi File JSON

---
+++ Demo/Demo/Controllers/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Demo.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text.Json;
+
+namespace Demo.Controllers
+{
+    public class StudentController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Student sv, string type)
+        {
+            if (type == "JSON")
+            {
+                var jsonString = JsonSerializer.Serialize(sv);
+                var pathFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "sv.json");
+                System.IO.File.WriteAllText(pathFile, jsonString);
+            }
+            else if(type == "TXT")
+            {
+
+            }
+            return View();
+        }
+
+        public IActionResult DocFileJson()
+        {
+            var pathFile = Path.Combine(Directory.GetCurrentDirectory(), "Data", "sv.json");
+            var jsonString = System.IO.File.ReadAllText(pathFile);
+            var svModel = JsonSerializer.Deserialize<Student>(jsonString);
+
+            return View("Index", svModel);
+        }
+    }
+}
---
+++ Demo/Demo/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Demo.Models
+{
+    public class Student
+    {
+        [Display(Name ="Mã sinh viên")]
+        public string StudentId { get; set; }
+        [Display(Name = "Họ tên")]
+        public string FullName { get; set; }
+        [Display(Name = "Điểm")]
+        public double Mark { get; set; }
+    }
+}
