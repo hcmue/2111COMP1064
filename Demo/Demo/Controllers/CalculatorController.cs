36f1d70ca234b8ddc884f5963b83950ee944b5a8
Hien Luong <hyhien@gmail.com>
2021-09-16 08:49:17 +0700

    Demo May tinh - Lab 01

---
+++ Demo/Demo/Controllers/CalculatorController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult XuLyTinhToan(string PhepToan, double SoHangA, double SoHangB)
+        {
+            var ketQua = 0.0;
+            switch (PhepToan)
+            {
+                case "+": ketQua = SoHangA + SoHangB; break;
+                case "-": ketQua = SoHangA - SoHangB; break;
+                case "*": ketQua = SoHangA * SoHangB; break;
+                case "/": ketQua = SoHangA / SoHangB; break;
+                case "^": ketQua = Math.Pow(SoHangA, SoHangB); break;
+                case "%": ketQua = SoHangA % SoHangB; break;
+            }
+
+            //return Content($"{SoHangA} {PhepToan} {SoHangB} = {ketQua}");
+            ViewBag.A = SoHangA;
+            ViewBag.B = SoHangB;
+            ViewBag.PhepToan = PhepToan;
+            ViewBag.KQ = ketQua;
+            return View("Index");
+        }
+    }
+}
