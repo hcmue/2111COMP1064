ebcb128e90809a165dc311ab38ca836d344d2842
Hien Luong <hyhien@gmail.com>
2021-10-21 07:28:21 +0700

    Init DangKyThanhVien

---
+++ WebApplication1/WebApplication1/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    public class DemoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        
+        [HttpGet]
+        public IActionResult DangKyThanhVien()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult XuLyDangKyThanhVien()
+        {
+            return View();
+        }
+    }
+}
