bffab826cc2c89996b4d53942f5a48394b8ddde1
Hien Luong <hyhien@gmail.com>
2021-10-21 08:40:01 +0700

    Remote validate

--- WebApplication1/WebApplication1/Controllers/DemoController.cs
+++ WebApplication1/WebApplication1/Controllers/DemoController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.Models;
 
 namespace WebApplication1.Controllers
 {
@@ -16,7 +18,18 @@ namespace WebApplication1.Controllers
         [HttpGet]
         public IActionResult DangKyThanhVien()
         {
+            var chuoiNgauNhien = MyTools.PhatSinhNgauNhien();
+            //Lưu số ngẫu nhiên trên Server dùng Session
+            HttpContext.Session.SetString("MaBaoMat", chuoiNgauNhien);
+
+            ViewBag.ChuoiNgauNhien = chuoiNgauNhien;
             return View();
+        }
+
+        public IActionResult KiemTraMaBaoMat(string MaBaoMat)
+        {
+            var checkMatch = HttpContext.Session.GetString("MaBaoMat") == MaBaoMat;
+            return Content(checkMatch ? "true" : "false");
         }
 
         [HttpPost]
---
+++ WebApplication1/WebApplication1/Models/MyTools.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Models
+{
+    public class MyTools
+    {
+        public static string PhatSinhNgauNhien(int length = 5)
+        {
+            var pattern = "zaqwsxedcrfvtgbyhnujmiklop[]1234567890<>?";
+            var random = new Random();
+            var sb = new StringBuilder();
+            for(var i = 0; i < length; i++)
+            {
+                sb.Append(pattern[random.Next(0, pattern.Length)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
